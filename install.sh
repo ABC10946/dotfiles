a2bef6338a7bd8ff18e423fe763047dd2b949b20
ABC10946 <abctech10946@gmail.com>
2022-03-21 16:01:54 +0900

    Plugインストールを自動化

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
-ln -sf $HOME/.dotfiles/vim $HOME/.vim
 ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
