3409675bd199f6f7b95593a7bc59c14a03af26aa
ABC10946 <abctech10946@gmail.com>
2022-10-10 16:22:31 +0900

    neovimのプラグイン設定の自動化

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
+if [ ! -d $HOME/.config/nvim/plugged ]; then
+	mkdir -p $HOME/.config/nvim/plugged
+fi
+
+curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sf $HOME/.dotfiles/vimrc $HOME/.vimrc
+ln -sf $HOME/.dotfiles/vimrc $HOME/.config/nvim/init.vim
 ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
 ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+
