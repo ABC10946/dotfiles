625746267498c60103c5740d863053e6040902ed
ABC10946 <abctech10946@gmail.com>
2022-11-25 16:27:31 +0900

    ADD: install deno automatically

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
-if [ ! -d $HOME/.config/nvim/plugged ]; then
-	mkdir -p $HOME/.config/nvim/plugged
+#!/bin/bash
+
+if [ ! -d "$HOME"/.config/nvim/plugged ]; then
+	mkdir -p "$HOME"/.config/nvim/plugged
 fi
 
-curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
+curl -fLo "$HOME"/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sf $HOME/.dotfiles/vimrc $HOME/.config/nvim/init.vim
-ln -sf $HOME/.dotfiles/bashrc $HOME/.bashrc
-ln -sf $HOME/.dotfiles/zshrc $HOME/.zshrc
-ln -sf $HOME/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf "$HOME"/.dotfiles/vimrc "$HOME"/.config/nvim/init.vim
+ln -sf "$HOME"/.dotfiles/bashrc "$HOME"/.bashrc
+ln -sf "$HOME"/.dotfiles/zshrc "$HOME"/.zshrc
+ln -sf "$HOME"/.dotfiles/tmux.conf "$HOME"/.tmux.conf
 
+command -v deno
+is_deno_installed=$?
+if [[ $is_deno_installed == 1 ]]; then
+	echo deno installing...
+	curl -fsSL https://deno.land/x/install/install.sh | sh
+	echo deno install complete
+fi
